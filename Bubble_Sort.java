9368c24f4e0b0206dc7c9544b729b8cc99ddb457
vidhusngh <62415244+vidhusngh@users.noreply.github.com>
2021-01-08 20:05:25 +0530

    Add files via upload

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -16,6 +16,26 @@ for(int i=0;i<n-1;i++) {
 	
 }
 }
+static void bubble_optimital(int array[]) {
+	boolean a=false;
+	int n=array.length;
+	int temp;
+	for(int i=0;i<n-1;i++) {
+		for(int j=0;j<n-i-1;j++) {
+			if(array[j]>array[j+1]) {
+				temp=array[j];
+				array[j]=array[j+1];
+				array[j+1]=temp;
+				a=true;
+			}
+			temp=0;
+			if(a==false)
+				break;
+		}
+		
+	}
+}
+
 public static void main(String args[]) {
   
   Scanner sc=new Scanner(System.in);
@@ -27,6 +47,7 @@ public static void main(String args[]) {
 	  arr[k]=sc.nextInt();
   }
   bubble(arr);
+ // bubble_optimital(arr);
   for(int i:arr) {
 	  System.out.print(i);
   }
