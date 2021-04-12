d7221d6c8e33ca3c6f4641d7f4fc2f2c3f6c96f3
vidhusngh <62415244+vidhusngh@users.noreply.github.com>
2021-04-12 16:22:38 +0530

    Create HashSet.java

---
+++ HashSet.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Iterator;
+//HashSet in java
+public class hss {
+public static void main(String []arg) {
+	HashSet<Integer> hs=new HashSet<Integer>();
+hs.add(10);
+hs.add(15);
+hs.add(25);
+hs.add(30);
+//add function adds the item and return true. if it is already present it returns false
+System.out.println(hs);
+hs.remove(15);
+System.out.println(hs.contains(25));
+Iterator<Integer> i=hs.iterator();
+while(i.hasNext()) {
+	System.out.print(i.next()+" ");
+}
+System.out.println();
+//No gurantee of order in iterator. Any item can be printed at any time
+System.out.println(hs.size());
+for(int j:hs) {
+	System.out.print(j + " ");
+}
+System.out.println();
+System.out.println(hs.isEmpty());
+hs.clear();
+
+System.out.println(hs.isEmpty());
+}
+
+
+}
